f9c705528611a40eccd94b9e3c652d40ac018018
repo-maru <betatest@tutamail.com>
2025-06-28 17:39:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=XVUGkZAJ#9s5arXLTJfvV-KSux8oYUFgldUU6f8bmLBPAeNqhPEM' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=qJ0mTIqa#TB5GveQ-BDrU7g8dPDZ33s7BIhRRfyXVAu0qkM-Zv4o' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
